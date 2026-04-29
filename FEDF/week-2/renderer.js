a8df59ce9e525a8cb5d97f036a10ca4d48480ad9
sai kiran <abburisaikiran4408@gmail.com>
2026-04-29 14:30:29 +0530

    Add files via upload

---
+++ FEDF/week-2/renderer.js
@@ -0,0 +1,11 @@
+let previousVDOM = ""; 
+export function render(vdom, container) {
+    if (vdom !== previousVDOM) { 
+        container.innerHTML = vdom;
+        previousVDOM = vdom; 
+        console.log("UI Updated"); 
+    } else {
+        console.log("No Change"); 
+    } 
+}
+
